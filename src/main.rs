29171d2393274e1d7539f12fe3c34fe74f1f0e0c
datewat <thomaszimmer524@gmail.com>
2022-05-29 18:56:25 +0200

    very basic argument handling and printing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        return panic!("Too many or too little arguments provided.\tUsage: 'mt <file/dir>'");
+    }
+
+    println!("{:?}", args);
 }
