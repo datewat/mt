362f46e87050247f5e73917ecd9708d81c465b5d
datewat <thomaszimmer524@gmail.com>
2022-05-29 19:43:08 +0200

    removed return from panic & unwrapped &p

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ fn main() {
     let args : Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        return panic!("Too many or too little arguments provided.\tUsage: 'mt <file/dir>'");
+        panic!("Too many or too little arguments provided.\tUsage: 'mt <file/dir>'");
     }
 
     println!("{:?}", args);
 
     let p = PathBuf::from(&args[1]);
-    println!("{:?}", fs::canonicalize(&p));
+    println!("{:?}", fs::canonicalize(&p).unwrap());
 }
