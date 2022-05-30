c362605a2dd4e0842962be86644b4124c9465f2d
datewat <thomaszimmer524@gmail.com>
2022-05-30 18:20:09 +0200

    exits if file does not exists

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,19 @@ use std::fs::File;
 use std::path;
 use std::path::PathBuf;
 use std::env;
+use std::path::Path;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        panic!("Too many or too little arguments provided.\tUsage: 'mt <file/dir>'");
+        panic!("Too many or too little arguments provided.\nUsage: 'mt <file/dir>'");
     }
 
     println!("{:?}", args);
 
     let p = PathBuf::from(&args[1]);
-    println!("{:?}", fs::canonicalize(&p).unwrap());
+    if !(Path::new(&p).exists()) {
+        panic!("File '{}' does not exist", args[1]);
+    }
 }
