efa36c5af6eca28634215711232b3a4a08af3b91
hyperbel <thomaszimmer524@gmail.com>
2022-06-05 14:55:35 +0200

    move trashfile into ~/.local/share/Trash/info/

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ fn main() {
     generate_trash_info_file(p);
 }
 
+fn move_file_to_trash() {
+
+}
+
 fn generate_trash_info_file(file: PathBuf) {
     let trashinfo_file_name: &String= &format!("{}.trashinfo", file.display());
 
@@ -27,17 +31,17 @@ fn generate_trash_info_file(file: PathBuf) {
     let trash_info = File::create(trashinfo_file_name);
     println!("{} file created", trashinfo_file_name);
 
-    fill_trash_info(trash_info.unwrap(), file);
-
-    move_trash_info(PathBuf::from(trashinfo_file_name));
-}
-
-fn fill_trash_info(mut file: File, path: PathBuf) {
-    //date format: yyyy-mm-ddThh:mm:ss
-    let date = Local::now().format("%Y-%m-%dT%H:%M:%S");
-
     let _homedir = env::var_os("HOME").unwrap();
     let homedir = _homedir.to_str().unwrap();
+
+    fill_trash_info(trash_info.as_ref().unwrap(), file);
+
+    move_trash_info(PathBuf::from(trashinfo_file_name), trash_info.unwrap(), homedir);
+}
+
+fn fill_trash_info(mut file: &File, path: PathBuf) {
+    //date format: yyyy-mm-ddThh:mm:ss
+    let date = Local::now().format("%Y-%m-%dT%H:%M:%S");
 
     let _pwd = env::var_os("PWD").unwrap();
     let pwd = _pwd.to_str().unwrap();
@@ -49,9 +53,9 @@ fn fill_trash_info(mut file: File, path: PathBuf) {
     file.write_all(first_line.as_bytes());
     file.write_all(second_line.as_bytes());
     file.write_all(third_line.as_bytes());
-
 }
 
-fn move_trash_info(file: PathBuf) {
-
+fn move_trash_info(path: PathBuf, file: File, hdir: &str) {
+    let t_path = format!("{hdir}/.local/share/Trash/info/{}", path.display());
+    fs::rename(path, t_path);
 }
