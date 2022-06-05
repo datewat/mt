e002e08632bbd254621a847ebd302cec4638aca8
hyperbel <thomaszimmer524@gmail.com>
2022-06-05 12:50:42 +0200

    fill trash info function created and cleaned up uses

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::path::PathBuf;
-use std::env;
-use std::path::Path;
+use std::{fs, fs::File, path::{Path, PathBuf}, env};
 use chrono::Local;
 
 fn main() {
@@ -18,9 +16,25 @@ fn main() {
     generate_trash_info_file(p);
 }
 
-fn generate_trash_info_file(file : PathBuf) {
+fn generate_trash_info_file(file: PathBuf) {
+    let trashinfo_file_name: &String= &format!("{}.trashinfo", file.display());
+
+    if fs::metadata(trashinfo_file_name).is_ok() {
+        println!("{} file exists", trashinfo_file_name);
+        return;
+    }
+
+    let trash_info = File::create(trashinfo_file_name);
+    println!("{} file created", trashinfo_file_name);
+
+    fill_trash_info(trash_info);
+}
+
+fn fill_trash_info(file: File) {
     //date format: yyyy-mm-ddThh:mm:ss
     let date = Local::now().format("%Y-%m-%dT%H:%M:%S");
 
-    println!("{}", date);
+    let first_line = "[Trash Info]";
+    let second_line = format!("Path=");
+    let third_line = format!("DeletionDate={}", date);
 }
